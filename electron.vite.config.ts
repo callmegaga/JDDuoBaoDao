65836438c0d7cd85fbd90ca8fc8ab5864f9835a2
王影 <wangying@everhigh.com.cn>
2023-07-29 17:16:17 +0800

    chore(style): change some file's codestyle

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,20 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import vue from '@vitejs/plugin-vue'
+import {resolve} from "path";
+import {defineConfig, externalizeDepsPlugin} from "electron-vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+    main: {
+        plugins: [externalizeDepsPlugin()]
     },
-    plugins: [vue()]
-  }
-})
+    preload: {
+        plugins: [externalizeDepsPlugin()]
+    },
+    renderer: {
+        resolve: {
+            alias: {
+                "@renderer": resolve("src/renderer/src")
+            }
+        },
+        plugins: [vue()]
+    }
+});
